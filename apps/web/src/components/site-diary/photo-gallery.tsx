71d11a69001a80a46ef977b95ca6933f14c5f957
John Patrick Agustin <agustin.johnpatrick@gmail.com>
2025-11-18 14:33:48 +0800

    Minor css changes for photo gallery

--- apps/web/src/components/site-diary/photo-gallery.tsx
+++ apps/web/src/components/site-diary/photo-gallery.tsx
@@ -10,12 +10,12 @@ const PhotoGallery: React.FC<PhotoGalleryProps> = ({ photos }) => {
   return (
     <div className="flex flex-wrap gap-2">
       {photos.map((src, i) => (
-        <img
+        <div
           key={i}
-          src={src}
-          alt={`photo-${i}`}
-          className="h-32 w-32 rounded-md object-cover"
-        />
+          className="aspect-square w-[30%] max-w-[120px] overflow-hidden rounded-lg"
+        >
+          <img src={src} alt="" className="h-full w-full object-cover" />
+        </div>
       ))}
     </div>
   );
