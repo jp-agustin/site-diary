66538fd2840ecf4969e1868a32bf0d4f9597be04
John Patrick Agustin <agustin.johnpatrick@gmail.com>
2025-11-19 15:29:36 +0800

    Standardize date display

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const formatDate = (rawDate?: string) => {
+  if (!rawDate) return 'N/A';
+  const dateObj = new Date(rawDate);
+  return new Intl.DateTimeFormat('en-US', {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  }).format(dateObj);
+};
